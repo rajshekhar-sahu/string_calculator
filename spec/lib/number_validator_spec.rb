5945d719fbbc5eeacd91d0da51c6955b0823b2bf
Rajshekhar Sahu <rajshekharsahu07656@gmail.com>
2025-06-12 15:16:13 +0530

    Setup specs for number validator

---
+++ spec/lib/number_validator_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../../lib/number_validator'
+
+RSpec.describe NumberValidator do
+  let(:input) { [] }
+
+  subject { NumberValidator.call(input) }
+
+  describe '#call' do
+    context "When input is empty array" do
+      it "returns empty array" do
+        expect(subject).to eq([])
+      end
+    end
+  end
+end
