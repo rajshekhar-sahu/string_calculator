f414752518e04a6cedec35bf61a1219e53fcf52c
Rajshekhar Sahu <rajshekharsahu07656@gmail.com>
2025-06-13 12:40:38 +0530

    Refactor string parser

--- lib/string_parser.rb
+++ lib/string_parser.rb
@@ -2,15 +2,19 @@ class StringParser
   def self.call(input)
     return [] if input.strip.empty?
 
-    delimiter = /[\n,]+/
+    delimiter, input = extract_delimiter_and_body(input)
+    input.split(delimiter).map(&:to_i).select { |num| num <= 1000 }
+  end
 
+  private
+
+  def self.extract_delimiter_and_body(input)
     if input.start_with?("//")
       parts = input.split("\n", 2)
       custom_delim = parts[0][2..]
-      delimiter = Regexp.new(Regexp.escape(custom_delim))
-      input = parts[1]
+      return Regexp.new(Regexp.escape(custom_delim)), parts[1]
+    else
+      return /[\n,]+/, input
     end
-
-    input.split(delimiter).map(&:to_i).select { |num| num <= 1000 }
   end
 end
